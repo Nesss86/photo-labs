8e3be4b96cb9561398ea777397706501994c0e31
Nesss86 <Nessymonster86@gmail.com>
2025-01-09 17:42:08 -0500

    Added styling to PhotoListItem component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,25 +1,30 @@
 import React from "react";
-
 import "../styles/PhotoListItem.scss";
 
-
-const PhotoListItem = ({id, location, imageSource, username, profile}) => {
+const PhotoListItem = ({ id, location, imageSource, username, profile }) => {
   return (
-    <div className="photo-list-item" id={`photo-${id}`}>
-      <img className="photo-list-item_image" src={imageSource} alt={`Photo by ${username}`}/>
-      <div className="photo-list-item_details">
+    <div className="photo-list__item" id={`photo-${id}`}>
+      <img
+        className="photo-list__image"
+        src={imageSource}
+        alt={`Photo by ${username}`}
+      />
+      <div className="photo-list__user-details">
         <img
-          className="photo-list-item_profile"
+          className="photo-list__user-profile"
           src={profile}
           alt={`${username}'s profile`}
         />
-        <p className="photo-list-item_username">{username}</p>
-        <p className="photo-list-item_location">
-          {location.city}, {location.country}
-        </p>  
-   </div>
-  </div>
+        <div>
+          <p className="photo-list__user-info">{username}</p>
+          <p className="photo-list__user-location">
+            {location.city}, {location.country}
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
 export default PhotoListItem;
+
