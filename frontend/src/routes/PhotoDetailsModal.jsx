bb0364b7502af75cd3cfa9c6f8aa66acb2392267
Nesss86 <Nessymonster86@gmail.com>
2025-01-12 19:49:42 -0500

    Passed relevant details about selected photo to the modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../styles/PhotoDetailsModal.scss";
 import closeSymbol from "../assets/closeSymbol.svg";
 
 const PhotoDetailsModal = ({ photo, onClose }) => {
+  // Log photo details to the console
+  useEffect(() => {
+    if (photo) {
+      console.log("Selected Photo Details:", photo);
+    }
+  }, [photo]);
+
   return (
     <div className="photo-details-modal" onClick={onClose}>
-      <div className="photo-details-modal__content" onClick={(e) => e.stopPropagation()}>
+      <div
+        className="photo-details-modal__content"
+        onClick={(e) => e.stopPropagation()}
+      >
         <button className="photo-details-modal__close-button" onClick={onClose}>
           <img src={closeSymbol} alt="close symbol" />
         </button>
-        {/* Content placeholder */}
-        <h1>{photo?.user.name}&#39;s Photo</h1>
-        <p>{photo?.location.city}, {photo?.location.country}</p>
+        {/* Placeholder content */}
+        <p>Modal content goes here...</p>
       </div>
     </div>
   );
 };
 
 export default PhotoDetailsModal;
+
 
